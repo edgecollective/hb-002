c883c54f6799e58d60af544a5ff04ab4d81c0c94
Don Blair <donblair@gmail.com>
2017-10-19 20:52:14 -0400

    update

---
+++ ctd/ctd.ino
@@ -0,0 +1,151 @@
+/*
+ Fading
+
+ This example shows how to fade an LED using the analogWrite() function.
+
+ The circuit:
+ * LED attached from digital pin 9 to ground.
+
+ Created 1 Nov 2008
+ By David A. Mellis
+ modified 30 Aug 2011
+ By Tom Igoe
+
+ http://www.arduino.cc/en/Tutorial/Fading
+
+ This example code is in the public domain.
+
+freq code:
+https://gist.github.com/jdneo/43be30d85080b175cb5aed3500d3f989
+
+ */
+
+const int selectPins[3] = {9, 6, 11}; // S0, S1, S2
+
+#define MUXPIN 6
+
+#define FREQUENCY 30000 
+#define LED_PIN 13  
+
+#define CPU_HZ 48000000
+#define TIMER_PRESCALER_DIV 1024
+
+void startTimer(int frequencyHz);
+void setTimerFrequency(int frequencyHz);
+void TC3_Handler();
+
+bool isLEDOn = false;
+
+void setup() {
+
+analogReadResolution(12);
+
+// conducitivity enabled by default
+
+  //enable pin for mux
+  //pinMode(12, OUTPUT);
+  //digitalWrite(12,LOW);
+
+  // input pin
+  pinMode(LED_PIN, OUTPUT);
+ startTimer(FREQUENCY);
+  
+  Serial.begin(9600);
+
+  // Set up the select pins, as outputs
+   for (int i=0; i<3; i++)
+  {
+    pinMode(selectPins[i], OUTPUT);
+    digitalWrite(selectPins[i], LOW);
+  }
+  
+}
+
+void setTimerFrequency(int frequencyHz) {
+  int compareValue = (CPU_HZ / (TIMER_PRESCALER_DIV * frequencyHz)) - 1;
+  TcCount16* TC = (TcCount16*) TC3;
+  // Make sure the count is in a proportional position to where it was
+  // to prevent any jitter or disconnect when changing the compare value.
+  TC->COUNT.reg = map(TC->COUNT.reg, 0, TC->CC[0].reg, 0, compareValue);
+  TC->CC[0].reg = compareValue;
+  Serial.println(TC->COUNT.reg);
+  Serial.println(TC->CC[0].reg);
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+}
+
+void startTimer(int frequencyHz) {
+  REG_GCLK_CLKCTRL = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC2_TC3) ;
+  while ( GCLK->STATUS.bit.SYNCBUSY == 1 ); // wait for sync
+
+  TcCount16* TC = (TcCount16*) TC3;
+
+  TC->CTRLA.reg &= ~TC_CTRLA_ENABLE;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Use the 16-bit timer
+  TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Use match mode so that the timer counter resets when the count matches the compare register
+  TC->CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  // Set prescaler to 1024
+  TC->CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+
+  setTimerFrequency(frequencyHz);
+
+  // Enable the compare interrupt
+  TC->INTENSET.reg = 0;
+  TC->INTENSET.bit.MC0 = 1;
+
+  NVIC_EnableIRQ(TC3_IRQn);
+
+  TC->CTRLA.reg |= TC_CTRLA_ENABLE;
+  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+}
+
+void TC3_Handler() {
+  TcCount16* TC = (TcCount16*) TC3;
+  // If this interrupt is due to the compare register matching the timer count
+  // we toggle the LED.
+  if (TC->INTFLAG.bit.MC0 == 1) {
+    TC->INTFLAG.bit.MC0 = 1;
+    // Write callback here!!!
+    digitalWrite(LED_PIN, isLEDOn);
+    isLEDOn = !isLEDOn;
+  }
+}
+void loop() {
+
+int numSamples=3;
+int sensorValue=0;
+
+selectMuxPin(MUXPIN);
+for (int i=0;i<numSamples;i++) {
+sensorValue += analogRead(A0);
+ //Serial.println(sensorValue);
+ delay(200);  
+}
+
+Serial.println(float(sensorValue)/numSamples);
+  
+}
+
+void selectMuxPin(byte pin)
+{
+  if (pin > 7) return; // Exit if pin is out of scope
+  for (int i=0; i<3; i++)
+  {
+    if (pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
+  }
+}
+
+
+
+
+
